c97e53d2049618ce2bb1162b85721f611e547616
AlperenYcdl <yucedalalper@gmail.com>
2022-11-29 19:43:16 +0100

    eskil

--- src/lambda_practice/Lambda02.java
+++ src/lambda_practice/Lambda02.java
@@ -9,7 +9,7 @@ public class Lambda02 {
 
     public static void main(String[] args) {
 
-        List<String> list=new ArrayList<>();
+        List<String> list = new ArrayList<>();
         list.add("Elma");
         list.add("portakal");
         list.add("uzum");
@@ -33,53 +33,56 @@ public class Lambda02 {
     }
 
     //S1: list elemanlarını ilk harf buyuk gerisi kucuk sekılde listeleyelim
-    public static List<String> ilkHarfBykGerisiKckList(List<String>l){
+    public static List<String> ilkHarfBykGerisiKckList(List<String> l) {
         return
                 l.stream().
-                        map(t->t.substring(0,1).toUpperCase()+t.substring(1).toLowerCase()).
+                        map(t -> t.substring(0, 1).toUpperCase() + t.substring(1).toLowerCase()).
                         collect(Collectors.toList());
     }
+
     // S2: ilk harfi e ve ya c olanlari listeleyelim
-    public static List<String> ecIleBaslayanList(List<String>liste){
+    public static List<String> ecIleBaslayanList(List<String> liste) {
         return
                 liste.stream().
-                        filter(t->t.toLowerCase().startsWith("e") || t.toLowerCase().startsWith("c")).
+                        filter(t -> t.toLowerCase().startsWith("e") || t.toLowerCase().startsWith("c")).
                         collect(Collectors.toList());
     }
+
     //S3: tum stringlerin basina ve sonuna yildiz ekleyerek yazdiralim
-    public static void yildizliYaz(List<String>liste){
+    public static void yildizliYaz(List<String> liste) {
         liste.
                 stream().
-                map(t->"*"+t+"*").forEach(Utils::yazString);
+                map(t -> "*" + t + "*").forEach(Utils::yazString);
 
     }
 
     //S4 : icinde e olanlardan yeni bir list olusturunuz
 
-    public static List<String> eOlanList(List<String>liste){
+    public static List<String> eOlanList(List<String> liste) {
         return
                 liste.stream().
-                        filter(t->t.toLowerCase().
+                        filter(t -> t.toLowerCase().
                                 contains("e")).collect(Collectors.toList());
     }
+
     //S5: tum 'l' leri silelim yazdiralim
-    public static void lLeriSil(List<String>liste){
+    public static void lLeriSil(List<String> liste) {
         liste.stream().
-                map(t->t.replace("l","")).
+                map(t -> t.replace("l", "")).
                 forEach(Utils::yazString);
     }
+
     //S6: List elemanarını 2.harfe gore sıralayıp
     //ilk 5 elemandan char sayısı en buyuk elemanı print
-    public static void ikiHrfSiraIlk5elChEnByk(List<String>liste){
+    public static void ikiHrfSiraIlk5elChEnByk(List<String> liste) {
         System.out.println(liste.
                 stream().
                 sorted(Comparator.comparing(t -> t.charAt(1))).//list elemanlarını 2.harfe gore sırala
                         limit(5).//ilk 5 eleman akısa devam etti
-                        sorted(Comparator.comparing(t->t.toString().length()).//akıstan gelen 5 elemanı uzunluklarına gore ters sıraladım
+                        sorted(Comparator.comparing(t -> t.toString().length()).//akıstan gelen 5 elemanı uzunluklarına gore ters sıraladım
                         reversed()).
                 findFirst());//akıstan gelen ılk elemanı aldım Optional[String]
     }
-
 
 
 }
--- src/lambda_practice/Lambda03.java
+++ src/lambda_practice/Lambda03.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lambda03 {
     public static void main(String[] args) {
@@ -14,6 +15,7 @@ public class Lambda03 {
 
         List<Apartman> kiralikDaireler=new ArrayList<>(Arrays.asList(daire1,daire2,daire3,daire4));
         doguKirayaSirala(kiralikDaireler);
+        System.out.println("kat2denCokKiraList(kiralikDaireler) = " + kat2denCokKiraList(kiralikDaireler));
 
     }
 
@@ -26,6 +28,11 @@ public class Lambda03 {
     }
     //kat sayısı 2den cok olan dairelerin kiralarını listeleyın
 
-
+    public static List<Integer> kat2denCokKiraList(List<Apartman>list){
+        return
+                list.stream().filter(t->t.getKatSayisi()>2).//kat sayısı 2den cok olan daıreler
+                        map(t->t.getKira()).
+                        collect(Collectors.toList());
+    }
 
 }
